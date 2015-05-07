5c54ae2f5d647713757f152417c97528c31b6780
sabour_f <sabour_f@epitech.eu>
2015-05-07 18:38:14 +0200

    Forgot a file

---
+++ AI/src/song.c
@@ -0,0 +1,54 @@
+/*
+** song.c for  in /home/sabour_f/rendu/ircbot/AI/src
+**
+** Made by Florian SABOURIN
+** Login   <sabour_f@epitech.net>
+**
+** Started on  Wed May  6 18:18:18 2015 Florian SABOURIN
+** Last update Wed May  6 18:18:18 2015 Florian SABOURIN
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include "database.h"
+
+t_song		*song_from_db(t_mapstring *res, unsigned int it)
+{
+  t_vector	*curr;
+  t_song	*ret;
+
+  ret = malloc(sizeof(t_song));
+  if (!ret)
+    return (NULL);
+  memset(ret, 0, sizeof(t_song));
+  curr = mapstring_findcstr(res, "id");
+  if ((curr = mapstring_findcstr(res, "id")))
+    ret->id = atoi(vector_at(curr, it));
+  if ((curr = mapstring_findcstr(res, "author")) && vector_at(curr, it))
+    ret->author = atoi(vector_at(curr, it));
+  if ((curr = mapstring_findcstr(res, "code")) && vector_at(curr, it))
+    if (!(ret->code = strdup(vector_at(curr, it))))
+      return (NULL + song_delete(ret, true));
+  if ((curr = mapstring_findcstr(res, "title")) && vector_at(curr, it))
+    if (!(ret->title = strdup(vector_at(curr, it))))
+      return (NULL + song_delete(ret, true));
+  if ((curr = mapstring_findcstr(res, "category")) && vector_at(curr, it))
+    if (!(ret->category = strdup(vector_at(curr, it))))
+      return (NULL + song_delete(ret, true));
+  return (ret);
+}
+
+int		song_delete(t_song *song, bool free_struct)
+{
+  free(song->title);
+  song->title = 0;
+  free(song->code);
+  song->code = 0;
+  free(song->category);
+  song->category = 0;
+  song->id = 0;
+  song->author = 0;
+  if (free_struct)
+    free(song);
+  return (0);
+}
