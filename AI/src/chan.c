0d04675306488c4f5a14816ec0cd805f9ae4bddd
sabour_f <sabour_f@epitech.eu>
2015-05-09 16:42:02 +0200

    Added song command

---
+++ AI/src/chan.c
@@ -0,0 +1,45 @@
+/*
+** chan.c for  in /home/sabour_f/rendu/ircbot/AI/src
+**
+** Made by Florian SABOURIN
+** Login   <sabour_f@epitech.net>
+**
+** Started on  Sat May  9 12:15:15 2015 Florian SABOURIN
+** Last update Sat May  9 12:15:15 2015 Florian SABOURIN
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include "database.h"
+
+t_chan		*chan_from_db(t_mapstring *res, unsigned int it)
+{
+  t_vector	*curr;
+  t_chan	*ret;
+
+  ret = malloc(sizeof(t_chan));
+  if (!ret)
+    return (NULL);
+  memset(ret, 0, sizeof(t_chan));
+  if ((curr = mapstring_findcstr(res, "id")))
+    ret->id = atoi(vector_at(curr, it));
+  if ((curr = mapstring_findcstr(res, "channel")) && vector_at(curr, it))
+    if (!(ret->channel = strdup(vector_at(curr, it))))
+      return (NULL + chan_delete(ret, true));
+  if ((curr = mapstring_findcstr(res, "server")) && vector_at(curr, it))
+    if (!(ret->server = strdup(vector_at(curr, it))))
+      return (NULL + chan_delete(ret, true));
+  return (ret);
+}
+
+int		chan_delete(t_chan *chan, bool free_struct)
+{
+  free(chan->channel);
+  chan->channel = 0;
+  free(chan->server);
+  chan->server = 0;
+  chan->id = 0;
+  if (free_struct)
+    free(chan);
+  return (0);
+}
---
+++ AI/src/command_song.c
@@ -0,0 +1,41 @@
+/*
+** command_song.c for  in /home/sabour_f/rendu/ircbot/AI/src
+**
+** Made by Florian SABOURIN
+** Login   <sabour_f@epitech.net>
+**
+** Started on  Sat May  9 15:39:25 2015 Florian SABOURIN
+** Last update Sat May  9 15:39:25 2015 Florian SABOURIN
+*/
+
+#include <errno.h>
+#include <stdlib.h>
+#include "luneth.h"
+
+int		command_song(t_bot *bot, t_ircconnection *co,
+			     t_luneth *luneth)
+{
+  t_song	*song;
+  char		*category;
+
+  (void)(bot);
+  category = strtok(NULL, " ");
+  if (!category)
+    {
+      song = database_select_random_song(luneth->db);
+      if (song)
+	{
+	  irc_msgf(co, co->cmd.args[0],
+	      "%s -> https://www.youtube.com/watch?v=%s [%s] : %s",
+	      co->cmd.prefixnick, song->code,
+	      song->category ? song->category : "",
+	      song->title ? song->title : "");
+	  song_delete(song, true);
+	}
+      else if (errno == ENOMEM)
+	return (1);
+      else if (irc_msgf(co, co->cmd.args[0], "Failed to find the song"))
+	return (1);
+    }
+  return (0);
+}
