3c2762305d8744bb723646331e4c3fbc1499fc4a
sabour_f <sabour_f@epitech.eu>
2015-09-16 19:18:40 +0200

    Added calc command

---
+++ AI/src/command_calc.c
@@ -0,0 +1,92 @@
+/*
+** command_calc.c for  in /home/sabour_f/rendu/ircbot/AI/src
+**
+** Made by Florian SABOURIN
+** Login   <sabour_f@epitech.net>
+**
+** Started on  Thu Sep  3 17:46:23 2015 Florian SABOURIN
+** Last update Thu Sep  3 17:46:23 2015 Florian SABOURIN
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "luneth.h"
+
+static char	*escape_doublequotes(const char *in)
+{
+  size_t	len;
+  const char	*curr;
+  char		*ret;
+  char		*it;
+  char		*res;
+
+  len = 0;
+  curr = in;
+  /* Count chars to escape, one char will be added for each one of them */
+  while (*(curr = strchrnul(curr, '"')))
+    {
+      ++len;
+      ++curr;
+    }
+  /* Add size of the string (including '\0') */
+  len += curr - in + 1;
+  ret = malloc(len);
+  if (!ret)
+    return (NULL);
+  it = ret;
+  curr = in;
+  while (*(res = strchrnul(curr, '"')))
+    {
+      /* Copy chars until a quote is met, then escape */
+      it = stpncpy(it, curr, res - curr);
+      it = stpcpy(it, "\"\"");
+      curr = res + 1;
+    }
+  /* Copy the rest of the string */
+  strcpy(it, curr);
+  return (ret);
+}
+
+#define CALC_BUFSIZ	512
+int		command_calc(t_bot *bot, t_ircconnection *co, t_luneth *luneth)
+{
+  static char	result[CALC_BUFSIZ];
+  size_t	sizeread;
+  FILE		*stream;
+  char		*str;
+  char		*execstr;
+  char		*escapedstr;
+
+  (void)(luneth);
+  (void)(bot);
+  /* Get and escape command */
+  str = strtok(NULL, "");
+  escapedstr = escape_doublequotes(str);
+  if (!escapedstr)
+    return (1);
+  if (asprintf(&execstr, "echo \"%s\" | bc", escapedstr) == -1)
+    return (1);
+  printf("command: %s -> %s\n", str, escapedstr);
+  printf("res: %s\n", execstr);
+  free(escapedstr);
+  /* Open pipe and read result */
+  stream = popen(execstr, "r");
+  free(execstr);
+  if (!stream)
+    {
+      perror("popen");
+      return (1);
+    }
+  sizeread = fread(result, 1, CALC_BUFSIZ, stream);
+  printf("Read %zu chars\n", sizeread);
+  if (pclose(stream) == -1)
+    return (1);
+  if (!sizeread)
+    return (irc_msg(co, co->cmd.args[0], "No result to display"));
+  result[sizeread] = '\0';
+  // Replace '\n' by spaces
+  str = result;
+  while ((str = strchr(str, '\n')))
+    *str = ' ';
+  return (irc_msg(co, co->cmd.args[0], result));
+}
--- AI/src/mess_privmsg.c
+++ AI/src/mess_privmsg.c
@@ -22,13 +22,16 @@ static int	log_message(t_ircconnection *co, t_luneth *luneth)
 const char	*cmds[] =
 {
   "ping", "action", "songid", "song", "pokemon", "cmd", "say", "shifumi",
+  "calc", ">",
   NULL
 };
 
 int	(* const fcttab[])(t_bot *, t_ircconnection *, t_luneth *) =
 {
   &command_ping, &command_action, &command_songid, &command_song,
-    &command_pokemon, &command_cmd, &command_say, &command_shifumi, NULL
+    &command_pokemon, &command_cmd, &command_say, &command_shifumi,
+    &command_calc, &command_calc,
+    NULL
 };
 
 static int	check_cmd(t_ircconnection *co, t_luneth *luneth, char *cmd)
