0296ee080f1916608c8a0a0dfadb2195713e5fc0
sabour_f <sabour_f@epitech.eu>
2015-09-24 15:06:01 +0200

    Added comments

--- main/include/ircbot.h
+++ main/include/ircbot.h
@@ -17,57 +17,71 @@
 # include "irc.h"
 # include "vector.h"
 
+// These are symbols to look for in the shared library
+
+// Returns a pointer that will be passed to all other functions
 # ifndef SYM_GET
 #  define SYM_GET	"irc_data_get"
 # endif
+// Upon receiving an IRC message
 # ifndef SYM_IRC
 #  define SYM_IRC	"irc_on_mess"
 # endif
+// Called periodically
 # ifndef SYM_PING
 #  define SYM_PING	"irc_nothing"
 # endif
+// Upon receiving something in standard input
 # ifndef SYM_STDIN
 #  define SYM_STDIN	"irc_stdin"
 # endif
+// Deallocate what SYM_GET allocated
 # ifndef SYM_DEL
 #  define SYM_DEL	"irc_data_delete"
 # endif
 
+// Convenience structure managing read/write file descriptors for select()
 typedef struct	s_net
 {
-  fd_set	rfds;
-  fd_set	wfds;
-  int		fdmax;
+  fd_set	rfds;	// readfds
+  fd_set	wfds;	// writefds
+  int		fdmax;	// nfds
 }		t_net;
 
+// The main bot structure (not the AI)
 typedef struct	s_bot t_bot;
 struct		s_bot
 {
-  struct timeval	timeref;
-  struct timeval	timeout;
-  struct timeval	*timeptr;
-  t_vector	conns;
-  t_net		net;
-  char		*so_name;
-  bool		running;
-  void		*dlhandle;
-  void		*handler_data;
+  struct timeval	timeref;  // select()'s timeout reference
+  struct timeval	timeout;  // select()'s actual timeout
+  struct timeval	*timeptr; // pointer to timeout (may be null)
+  t_vector	conns;          // array of connections
+  t_net		net;            // select()'s basic informations
+  char		*so_name;       // If nonnull, the next loop, the AI will be
+				// reloaded from this file
+  bool		running;        // true if running, false otherwise
+  void		*dlhandle;      // dlopen()'s handle
+  void		*handler_data;  // The data of the AI
+  // The SYM_GET pointer
   int		(*handler_fct)(t_bot *, t_ircconnection *, void *);
+  // The SYM_STDIN pointer
   int		(*handler_input_fct)(t_bot *, char *, void *);
+  // The SYM_PING pointer
   int		(*handler_nothing_fct)(t_bot *, void *);
 };
 
-int		bot_select(t_bot *bot);
+// Bot main functions
+int	bot_select(t_bot *bot);
 int	bot_connect(t_bot *bot, const char *hostname, unsigned short port,
 		    const char *nick, const char *user, const char *realname,
 		    const char *pass);
-int		bot_disconnect(t_bot *bot, size_t pos);
+int	bot_disconnect(t_bot *bot, size_t pos);
 
-  /* AI handling */
-int		loadAI(t_bot *bot, char *filename);
-void		unloadAI(t_bot *bot);
-int		handler_fct_none(t_bot *bot, t_ircconnection *co, void *dat);
-int		handler_input_fct_none(t_bot *bot, char *input, void *dat);
-int		handler_nothing_none(t_bot *bot, void *dat);
+// AI handling
+int	loadAI(t_bot *bot, char *filename);
+void	unloadAI(t_bot *bot);
+int	handler_fct_none(t_bot *bot, t_ircconnection *co, void *dat);
+int	handler_input_fct_none(t_bot *bot, char *input, void *dat);
+int	handler_nothing_none(t_bot *bot, void *dat);
 
 #endif /* !IRCBOT_H_ */
